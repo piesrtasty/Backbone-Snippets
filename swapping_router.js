e2658945f0720f4f61111beb499cdaac69a91fe6
virtually-ghetto <lukemhamilton@gmail.com>
2011-10-23 20:15:53 -0400

    Added view swapping router

---
+++ swapping_router.js
@@ -0,0 +1,19 @@
+Support.SwappingRouter = function(options)	{
+	Backbone.Router.apply(this, [options]);
+};
+
+_.extend(Support.SwappingRouter.prototype, Backbone.Router.prototype,	{
+	
+	swap: function(newView)	{
+		if (this.currentView && currentView.leave)	{
+			this.currentView.leave();
+		}
+		
+		this.currentView = newView;
+		this.currentView.render();
+		$(this.el).empty().append(this.currentView.el);
+	}
+	
+});
+
+Support.SwappingRouter.extend = Backbone.Router.extend;
