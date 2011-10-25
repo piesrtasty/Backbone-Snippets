537f352cdd3b7af8df1f70c03699957a95012b1a
virtually-ghetto <lukemhamilton@gmail.com>
2011-10-24 23:59:40 -0400

    refactored filter.js

--- models_and_collections/filters_and_sorting/filters/filters.js
+++ models_and_collections/filters_and_sorting/filters/filters.js
@@ -7,7 +7,30 @@ var Tasks = Backbone.Collection.extend({
 	url: '/tasks',
 	
 	complete: function()	{
-		
+		var filteredTasks = this.select(function(tasks)	{
+			return task.get('completed_at') !== null;
+		});
+		return new Tasks(filteredTasks);
 	}
+});
+
+// Let's refactor this a bit. Ideally, the filter functions will reuse logic already
+// defined in your model class ...
+
+var Task = Backbone.Model.extend({
+	isComplete: function()	{
+		return this.get('completed_at') !== null;
+	}
+});
+
+var Tasks = Backbone.Collection.extend({
+	model: Task,
+	url: '/tasks',
 	
+	complete: function()	{
+		var filteredTasks = this.select(function(task)	{
+			return task.isComplete();
+		});
+		return new Tasks(filteredTasks);
+	}
 });
