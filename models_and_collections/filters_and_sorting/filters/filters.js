6885f9ea3398469fc84dbf5071f57c48de0d0ae3
virtually-ghetto <lukemhamilton@gmail.com>
2011-10-25 00:20:50 -0400

    Added filterable collection mixin

--- models_and_collections/filters_and_sorting/filters/filters.js
+++ models_and_collections/filters_and_sorting/filters/filters.js
@@ -34,3 +34,107 @@ var Tasks = Backbone.Collection.extend({
 		return new Tasks(filteredTasks);
 	}
 });
+
+// Going further, notice that there are actually two concerns in this function
+// 1. The notion of filtering the collection
+// 2. The specific filtering criteria (i.e. task.isComplete())
+
+// Let's separate the two concerns here, and extract a filtered function...
+
+var Task = Backbone.Model.extend({
+	isComplete: function()	{
+		return this.get('completed_at') !== null;
+	}
+});
+
+var Tasks = Backbone.Collection.extend({
+	model: Task,
+	url: '/tasks',
+	
+	complete: function()	{
+		return this.filtered(function(task)	{
+			return task.isComplete();
+		});
+	},
+	
+	filtered: function(criteriaFunction)	{
+		return new Tasks(this.select(criteriaFunction));
+	}
+	
+})
+
+// Now we can extract this function into a reusable mixin, abstracting the Tasks
+// collection class using this.constructor...
+
+var FilterableCollectionMixin = {
+	filtered: function(criteriaFunction)	{
+		return new this.constructor(this.select(criteriaFunction))
+	}
+};
+
+var Task = Backbone.Model.extend({
+	isComplete: function()	{
+		return this.get('completed_at') != null;
+	}
+});
+
+var Tasks = Backbone.Collection.extend(_.extend({
+	model: Task,
+	url: '/tasks',
+	
+	complete: function()	{
+		return this.filtered(function(task)	{
+			return task.isComplete();
+		});
+	}
+}, FilterableCollectionMixin));
+
+/* PROPAGATING COLLECTION CHANGES */
+
+// The FilterableCollectionMixin, as we've written it, will produce a filtered collection
+// that does not update when the original collection is changed. To do so, bind to the
+// change, add, and remove events on the source collection, reapply the filter function,
+// and repopulate the filtered collection.
+
+var FilterableCollectionMixin = {
+	filtered: function(criteriaFunction)	{
+		var sourceCollection = this;
+		var filteredCollection = new this.constructor;
+		
+		var applyFilter = function()	{
+			filteredCollection.reset(sourceCollection.select(criteriaFunction));
+		};
+		
+		this.bind("change", applyFilter);
+		this.bind("add", applyFilter);
+		this.bind("remove", applyFilter);
+		
+		applyFilter();
+		
+		return filteredCollection;
+		
+	}
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
