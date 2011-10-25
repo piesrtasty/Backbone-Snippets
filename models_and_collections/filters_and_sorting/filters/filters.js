023f0eff45dced7147e640c9a9ac9f235d345e56
virtually-ghetto <lukemhamilton@gmail.com>
2011-10-24 23:46:42 -0400

    test

--- models_and_collections/filters_and_sorting/filters/filters.js
+++ models_and_collections/filters_and_sorting/filters/filters.js
@@ -1,4 +1,6 @@
-/* To filter a Backbone.Collection, like with Rails named scopes, define functions on your collections that filter by your criteria, using the "select" function from Underscore.js, and return new instances of the collection class. A first implementation might look as follows...*/
+/* To filter a Backbone.Collection, like with Rails named scopes, define functions on your
+ collections that filter by your criteria, using the "select" function from Underscore.js, and
+ return new instances of the collection class. A first implementation might look as follows...*/
 
 var Tasks = Backbone.Collection.extend({
 	model: Task,
